376e9acaf3e41ce99f54e8e0e08ad2b99bc8292a
Taufan <taufanremote@gmail.com>
2023-12-13 19:33:57 +0700

    Manual route testing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
