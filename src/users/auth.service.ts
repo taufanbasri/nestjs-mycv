e96e46784d858c96d291d943b3a57c1aacf2a26a
Taufan Prasetyo <taufanremote@gmail.com>
2023-12-17 08:52:26 +0700

    User signup with hashed password

---
+++ src/users/auth.service.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { promisify } from 'util';
+import { scrypt as _scrypt, randomBytes } from 'crypto';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.userService.find(email);
+
+    if (users.length) {
+      throw new BadRequestException('email in user');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.userService.create(email, result);
+
+    return user;
+  }
+
+  signin() {}
+}
